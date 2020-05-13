89814fdbdf9bf8fd247cea5a86e4aac68ec22d42
kerstihuqi <kersti_huqi@live.com>
2020-05-13 12:22:06 +0200

    Merge to master

--- Project ASP/e-shop/e-shop/Models/DatabaseModels/eshopContext.cs
+++ Project ASP/e-shop/e-shop/Models/DatabaseModels/eshopContext.cs
@@ -33,7 +33,7 @@ namespace e_shop.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server = (LocalDb)\\MSSQLLocalDB; Database = eshop; Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server = WINDOWS-GIQE8BS\SQLEXPRESS; Database = eshop; Trusted_Connection=True;");
             }
         }
 
