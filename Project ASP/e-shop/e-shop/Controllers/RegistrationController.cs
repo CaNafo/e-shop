0898f96184dd029c113a0afe2ec64cb8bb2cfd6d
kerstihuqi <kersti_huqi@live.com>
2020-05-13 21:06:43 +0200

    Registration V2

---
+++ Project ASP/e-shop/e-shop/Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using e_shop.Models;
+
+namespace e_shop.Controllers
+{
+    [Route("api/Registration")]
+    [ApiController]
+    public class RegistrationController : Controller
+    {
+        [HttpPost]
+        public ActionResult<IEnumerable<Users>> JsonStringBody([FromBody] RegistrationModel content)
+        {
+            using (var context = new eshopContext())
+            {
+                Users newUser = new Users();
+                {
+                    newUser.FirstName = content.FirstName;
+                    newUser.LastName = content.LastName;
+                    newUser.BirthDate = content.BirthDate;
+                    newUser.EMail = content.EMail;
+                    newUser.Password = content.Password;
+                };
+                context.Users.Add(newUser);
+                context.SaveChanges();
+
+                return new List<Users>();
+            }
+        }
+    }
+}
---
+++ Project ASP/e-shop/e-shop/Models/RegistrationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_shop.Models
+
+{
+    public class RegistrationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string EMail { get; set; }
+        public string Password { get; set; }
+
+    }
+}
